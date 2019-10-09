8066566bade07abe70ad8ff1d2496d120fb15dc2
afhare <alexandra.f.hare@gmail.com>
2019-10-09 09:43:35 -0500

    First seeds for database: task categories, aka 'Type' records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+types = Type.create([{name: "work"},{name: "personal"},{name: "other"},{name: "routine"},{name: "project"},{name: "random"}, {name: "essential"} ])
